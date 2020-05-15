a597983e4fa09dcfab3a8c0f40c8e397c3d43edf
minsung <rkdalstjd9@naver.com>
2020-05-15 23:15:25 +0900

    회원가입 로직

--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import ProfileSerializer
+
 
 def main(request):
     return HttpResponse("hello world")
+
+
+class CreateProfileView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data.get('profile')
+        if not data:
+            return Response({
+                'response': 'error',
+                'message': 'profile 파라미터가 없습니다.'
+            })
+        serializer = ProfileSerializer(data=data)
+        if serializer.is_valid():
+            profile = serializer.save()
+        else:
+            return Response({
+                'response': 'error',
+                'message': serializer.errors
+            })
+
+        return Response({
+            'response': 'success',
+            'message': serializer.data
+        })
