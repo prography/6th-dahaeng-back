20ab245e95c73d1d0c59abfad065245c847a0dab
minsung <rkdalstjd9@naver.com>
2020-05-15 23:04:12 +0900

    ProfileSerializer 생성

---
+++ project/core/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers as sz
+from django.contrib.auth import get_user_model
+
+
+class ProfileSerializer(sz.ModelSeializer):
+    password = sz.CharField(write_only=True)
+
+    def create(self, validated_data):
+        Profile = get_user_model()
+        password = validated_data.pop('password')
+        profile = Profile(**validated_data)
+        profile.set_password(password)
+        profile.save()
+        return profile
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'username',
+            'password',
+            'email',
+            'nickname'
+        ]
