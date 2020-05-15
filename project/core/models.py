59f74b5558e2507f0c69f8691222a87f80106912
minsung <rkdalstjd9@naver.com>
2020-05-15 22:25:21 +0900

    profile 모델 생성

--- project/core/models.py
+++ project/core/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    user_in_migrations = True
+
+    def create_user(self, username, email, nickname, password=None):
+        if not email:
+            raise ValueError('이메일은 필수입니다.')
+        # 가독성을 고려하여 kwargs 사용 안함
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            nickname=nickname
+        )
+        user.set_password(password)
+        user.status = '0'
+        user.role = '0'
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, nickname="", password=None):
+        user = self.create_user(
+            username=username,
+            password=password,
+            nickname=nickname
+        )
+        user.status = '1'
+        user.role = '10'
+        user.save(using=self._db)
+        return user
+
+
+class Profile(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    STATUS_CHOICES = (
+        ('0', '가입대기'),
+        ('1', '가입활성화'),
+        ('8', '블랙리스트'),
+        ('9', '탈퇴')
+    )
+
+    ROLE_CHOICES = (
+        ('0', '일반 유저'),
+        ('10', '관리자')
+    )
+
+    username = models.CharField(max_length=16, unique=True)
+    email = models.EmailField(max_length=50)
+    nickname = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, blank=True)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES, blank=True)
+
+    @property
+    def is_staff(self):
+        return self.role == '10'
+
+    @property
+    def is_superuser(self):
+        return self.role == '10'
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
