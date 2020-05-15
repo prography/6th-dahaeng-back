9d09f93d79e417f6c2661e91dcff1d5a6818929b
minsung <rkdalstjd9@naver.com>
2020-05-16 03:30:04 +0900

    login test

--- project/core/urls.py
+++ project/core/urls.py
@@ -3,6 +3,7 @@ from . import views
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
+    path('', views.login_test, name='login_test'),
     path('login/', obtain_jwt_token, name='login'),
     path('refresh/', refresh_jwt_token, name='refresh'),
     path('signup/', views.CreateProfileView.as_view(), name='signup'),
--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view
 from .serializers import ProfileSerializer
 from django.contrib.auth import get_user_model
 
@@ -43,3 +44,8 @@ class CreateProfileView(APIView):
             'response': 'success',
             'message': serializer.data
         })
+
+
+@api_view(['GET'])
+def login_test(request):
+    return Response({'message': '로그인 성공'})
