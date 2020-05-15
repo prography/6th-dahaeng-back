b3cd12276ba329b9cc0a1b2471dd258e8322b41c
minsung <rkdalstjd9@naver.com>
2020-05-16 02:32:39 +0900

    login, refresh endpoint 생성

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('', views.main, name="main"),
+    path('login/', obtain_jwt_token, name='login'),
+    path('refresh/', refresh_jwt_token, name='refresh'),
     path('signup/', views.CreateProfileView.as_view(), name='signup'),
 ]
