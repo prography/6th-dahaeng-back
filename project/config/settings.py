8f806f87103de6488a024cfbb5af73631e32eb14
minsung <rkdalstjd9@naver.com>
2020-05-26 00:00:06 +0900

    coustom permission

--- project/config/settings.py
+++ project/config/settings.py
@@ -129,7 +129,8 @@ AUTH_USER_MODEL = "core.Profile"
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'config.permissions.MyIsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
