728b905a9e0400673088a9c56dd1ded2344689d2
SungEunJang <suuuu101@naver.com>
2020-05-28 18:03:36 +0900

    유효성 검증방식 update 내부 구현으로 수정

--- project/record/serializers.py
+++ project/record/serializers.py
@@ -14,12 +14,6 @@ class PostSerializer(sz.ModelSerializer):
     profile = sz.SlugRelatedField(queryset=Profile.objects.all(), slug_field='email',)
     emotion = sz.ChoiceField(choices=Post.EMOTION_CHOICES, default='따')
     created_at = sz.DateField(default=date.today())
-    
-    def validate(self, data):
-        today = date.today()        
-        if not today == data['created_at']:
-            raise sz.ValidationError("지난 날짜는 수정할 수 없습니다.")
-        return data
 
     def create(self, validated_data):
         """
@@ -29,11 +23,16 @@ class PostSerializer(sz.ModelSerializer):
         return post
     
     def update(self, instance, validated_data):
-        instance.emotion = validated_data.get('emotion', instance.emotion)
-        instance.question = validated_data.get('question', instance.question)
-        instance.detail = validated_data.get('detail', instance.detail)
-        instance.save()
-        return instance
+        today = date.today()
+        created_at = instance.created_at
+        if not today == created_at:
+            raise sz.ValidationError({"response": "error", "message": "지난 날짜는 수정할 수 없습니다."})
+        else:
+            instance.emotion = validated_data.get('emotion', instance.emotion)
+            instance.question = validated_data.get('question', instance.question)
+            instance.detail = validated_data.get('detail', instance.detail)
+            instance.save()
+            return instance
     
     class Meta:
         model = Post
