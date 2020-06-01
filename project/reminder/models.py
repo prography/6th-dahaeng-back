9eee6716c9ee838794d4a2938ccf7b0323b5a9e3
minsung <rkdalstjd9@naver.com>
2020-06-01 18:29:56 +0900

    created_at 필드 추가

--- project/reminder/models.py
+++ project/reminder/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Reminder(models.Model):
     post = models.OneToOneField(Post, on_delete=models.CASCADE)
-    is_sent = models.BooleanField(default=False)
+    interval = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.post.profile}의 {self.post.created_at} remainder"
