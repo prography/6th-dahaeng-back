158a7355bc13978484a06dbe30a02d977f9ee1db
minsung <rkdalstjd9@naver.com>
2020-07-06 19:00:11 +0900

    Question max_length 255 제한

--- project/record/models.py
+++ project/record/models.py
@@ -13,7 +13,7 @@ def date_upload_to(instance, filename):
 
 
 class Question(models.Model):
-    content = models.CharField(max_length=512, unique=True)
+    content = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.content
