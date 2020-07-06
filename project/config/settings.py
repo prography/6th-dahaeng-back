3b2b6bdc4b288d87d0b67c054dd3183a0b825321
minsung <rkdalstjd9@naver.com>
2020-07-06 17:52:56 +0900

    mysql 세팅

--- project/config/settings.py
+++ project/config/settings.py
@@ -87,10 +87,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Dahaeng',
+        'USER': 'Dahaeng',
+        'PASSWORD': 'Dahaeng1!',
+        'HOST': 'dahaenginstance.chzp9ckdofcl.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
