2a8b9a333a6390baab5afe605f84d479aaf22b51
minsung <rkdalstjd9@naver.com>
2020-07-10 15:30:01 +0900

    merge conflict 수정

--- project/config/urls.py
+++ project/config/urls.py
@@ -7,10 +7,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('record/', include('record.urls')),
     path('reminder/', include('reminder.urls')),
-<<<<<<< HEAD
     path('notice/', include('notice.urls')),
-=======
     path('shop/', include('shop.urls')),
->>>>>>> 12166529d74c9a5d75cdf491a4ec961bd8bca39b
     path('api-auth/', include('rest_framework.urls')),
 ]
