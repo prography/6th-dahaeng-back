4cb9fd13c2d908142fe28c0767a08f70bdbfe49f
minsung <rkdalstjd9@naver.com>
2020-07-10 22:04:23 +0900

    S3 설정

--- project/config/settings.py
+++ project/config/settings.py
@@ -196,23 +196,23 @@ CRONJOBS = [
 CRONTAB_COMMAND_SUFFIX = '2>&1'
 
 
-# # AWS S3 Storage
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS S3 Storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# # AWS Access
-# AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
-# AWS_REGION = get_secret('AWS_REGION')
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
-#     AWS_STORAGE_BUCKET_NAME, AWS_REGION)
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_DEFAULT_ACL = 'public-read'
-# AWS_MEDIA_LOCATION = 'media'
-# MEDIA = 'http://%s/%s' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
+# AWS Access
+AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
+AWS_REGION = get_secret('AWS_REGION')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
+    AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_DEFAULT_ACL = 'public-read'
+AWS_MEDIA_LOCATION = 'media'
+MEDIA = 'http://%s/%s' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
