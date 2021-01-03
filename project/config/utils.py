2adef02c05043a270cec316628439a61b0310f28
shunnyjang <suuuu101@naver.com>
2021-01-03 21:59:23 +0900

    Modify to choose random color in Item model

--- project/config/utils.py
+++ project/config/utils.py
@@ -1,10 +1,14 @@
 from random import choice
+from core.ERROR.error_cases import GlobalErrorMessage401
+from shop.models import Item
 
-colors = ["A26C8F", "F8DB5C", "73A38F", "FF714D", "5CA1D2"]
 
-
-def random_color():
+def random_color() -> Item:
     """
         추후 조랭이 색깔을 정하기 위해서 사용이 되는 함수이다.
+        조랭이 컬러 추가에 대응하기 위해 RGB string 선택에서 Item선택으로 변경했다.
     """
-    return choice(colors)
+    colors = Item.objects.all().filter(item_type="jorang_color")
+    if colors:
+        return choice(colors)
+    raise GlobalErrorMessage401("아이템이 없습니다.")
